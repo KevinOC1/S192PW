1cdcb3056f835334744de1e7df097c6df03daeb5
KevinOC1 <kioc989@gmail.com>
2024-09-25 19:23:41 -0600

    add proyect

---
+++ intro_laravel/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//Ruta tipo GET
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
+
+
+//Ruta tipo VIEW
+Route ::view('/','welcome');
+
+route :: view('/form','formulario');
